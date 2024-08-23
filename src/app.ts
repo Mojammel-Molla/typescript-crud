80c35ca88961af840b8f1db256a0d74542ef5452
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-08-23 23:58:25 +0600

    Eslint and prettier setup done

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
-import express from 'express';
-const app = express();
-const port = 3000;
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+// const port = 3000;
 
-app.get('/', (_req, res) => {
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+
+export default app;
