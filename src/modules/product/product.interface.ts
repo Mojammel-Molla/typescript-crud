cab2ba21615abb9da48f865420f9310a962ad628
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-08-24 23:45:20 +0600

    Product interface created

---
+++ src/modules/product/product.interface.ts
@@ -0,0 +1,19 @@
+export type ShoeVariant = {
+  type: string;
+  value: string;
+};
+
+export type ShoeInventory = {
+  quantity: number;
+  inStock: boolean;
+};
+
+export type ShoeProduct = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: ShoeVariant[];
+  inventory: ShoeInventory;
+};
